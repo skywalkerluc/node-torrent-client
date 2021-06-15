9597d622d7a40e11dd30fe22f8410a10482a4dd8
skywalkerluc <lucas.182.ferreira@hotmail.com>
2021-06-15 14:12:48 -0300

    Start decoding bencode and adding readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Notes:
+
+### Http vs udp vs tcp
+The main reason that most trackers now use udp is that udp has better performance than http. Http is built on top of another protocol called tcp, which we’ll use later in the project when we start actually downloading files from peers.
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
 const fs = require('fs');
-const torrent = fs.readFileSync('puppy.torrent');
-console.log(torrent.toString('utf-8'));
+const bencode = require('bencode');
+
+// bencode
+const torrentBencode = fs.readFileSync('puppy.torrent');
+console.log(torrentBencode.toString('utf-8'));
+
+const torrent = bencode.decode(torrentBencode);
+console.log(torrent, 'torrentDecoded');
