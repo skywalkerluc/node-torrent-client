f7841571f944da4dff735154f08a522de5d37657
skywalkerluc <lucas.182.ferreira@hotmail.com>
2021-06-17 19:48:10 -0300

    WIP

--- src/__tests__/torrent-parser.test.js
+++ src/__tests__/torrent-parser.test.js
@@ -1,7 +1,17 @@
 const { open, size, infoHash } = require('../torrent-parser');
+const path = require('path');
 
 describe('torrent-parser scenarios', () => {
   test('open function should trigger bencode.decode ', () => {
+    const torrent = open(path.resolve(__dirname, '../../public/puppy.torrent'));
+
+  });
+
+  test('size function should return torrent size', () => {
+
+  });
+
+  test('infoHash function should trigger crypto create hash', () => {
 
   });
 });
